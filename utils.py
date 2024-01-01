07a8a13e6b281d67db6b1585688dc9a45c37bdb0
Man Chau <tsunman.chau.g@gmail.com>
2024-01-01 16:05:27 +0800

    Transformer

---
+++ utils.py
@@ -0,0 +1,23 @@
+from typing import List
+import torch
+from torch.nn.utils.rnn import pad_sequence
+
+class SpecialSymbols:
+    # Define special symbols and indices
+    UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
+    # Make sure the tokens are in order of their indices to properly insert them in vocab
+    special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+
+# helper function to club together sequential operations
+def sequential_transforms(*transforms):
+    def func(txt_input):
+        for transform in transforms:
+            txt_input = transform(txt_input)
+        return txt_input
+    return func
+
+# function to add BOS/EOS and create tensor for input sequence indices
+def tensor_transform(token_ids: List[int]):
+    return torch.cat((torch.tensor([BOS_IDX]),
+                      torch.tensor(token_ids),
+                      torch.tensor([EOS_IDX])))
