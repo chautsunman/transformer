581059b4536290d80345fd3f75553674ad149071
Man Chau <tsunman.chau.g@gmail.com>
2024-01-01 17:34:54 +0800

    Transformer

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 from typing import List
 import torch
-from torch.nn.utils.rnn import pad_sequence
 
 class SpecialSymbols:
     # Define special symbols and indices
@@ -18,6 +17,14 @@ def sequential_transforms(*transforms):
 
 # function to add BOS/EOS and create tensor for input sequence indices
 def tensor_transform(token_ids: List[int]):
-    return torch.cat((torch.tensor([BOS_IDX]),
+    return torch.cat((torch.tensor([SpecialSymbols.BOS_IDX]),
                       torch.tensor(token_ids),
-                      torch.tensor([EOS_IDX])))
+                      torch.tensor([SpecialSymbols.EOS_IDX])))
+
+def token_to_sentence(tokens, vocab_transform, replace_pad=False):
+    sentence = " ".join(vocab_transform.lookup_tokens(list(tokens)))
+    sentence = sentence.replace(SpecialSymbols.special_symbols[SpecialSymbols.BOS_IDX], "")
+    sentence = sentence.replace(SpecialSymbols.special_symbols[SpecialSymbols.EOS_IDX], "")
+    if replace_pad:
+        sentence = sentence.replace(SpecialSymbols.special_symbols[SpecialSymbols.PAD_IDX], "")
+    return sentence
